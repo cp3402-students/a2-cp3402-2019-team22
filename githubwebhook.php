5098e5977b72aa500e9835476c64ea2475cb32a8
Heylon White <heylon.white@my.jcu.edu.au>
2019-05-14 17:48:43 +1000

    Modified githubwebhook for staging server

--- githubwebhook.php
+++ githubwebhook.php
@@ -1,1 +1,7 @@
-<?php 'git pull';
+<?php
+echo "<b>Current working directory: </b>" . getcwd();
+$output = shell_exec("git pull 2>&1");
+echo "<h1>Git Pull</h1>" . "<pre>" . $output . "</pre>";
+
+$output2 = shell_exec("ls -l");
+echo "<h1>ls -l</h1>" . "<pre>" . $output2 . "</pre>";
