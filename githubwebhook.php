a9f9d8e6f42fd062b2dc08f71c9e6df5b45cd2fb
Heylon White <heylon.white@my.jcu.edu.au>
2019-05-22 14:58:20 +1000

    Add sudo git reset command to webhook

--- githubwebhook.php
+++ githubwebhook.php
@@ -1,5 +1,9 @@
 <?php
 echo "<b>Current working directory: </b>" . getcwd();
+
+$output3 = shell_exec("sudo git reset --hard origin/master 2>&1");
+echo "<h1>Git reset</h1><pre>" . $output3 . "</pre>"; 
+
 $output = shell_exec("git pull 2>&1");
 echo "<h1>Git Pull</h1>" . "<pre>" . $output . "</pre>";
 
